c1fa1dbcf8e0dd28b08296adf5e43bf8cdb76c9d
Yaroslav <yroslav070411@gmail.com>
2023-01-27 01:28:14 +0300

    add apiSlice

--- src/api/recommendedApiSlice.ts
+++ src/api/recommendedApiSlice.ts
@@ -3,4 +3,5 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const recommendedApiSlice = createApi({
   reducerPath: 'recommended',
   baseQuery: fetchBaseQuery({ baseUrl: `https://api.hh.ru/` }),
+  endpoints: (builder) => ({}),
 });
