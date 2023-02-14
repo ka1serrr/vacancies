622fb4377dc9516f9f387f9160f64281a90f0317
ka1serrr <86408343+ka1serrr@users.noreply.github.com>
2023-02-14 15:45:00 +0300

    Fixied fallback for Content

--- src/components/UI/LoaderComponent/LoaderComponent.tsx
+++ src/components/UI/LoaderComponent/LoaderComponent.tsx
@@ -1,23 +1,34 @@
-import React, {FC} from 'react';
-import ContentLoader from "react-content-loader"
+import React, { FC } from 'react';
+import ContentLoader from 'react-content-loader';
+import './loaderComponent.scss';
 
-export const LoaderComponent: FC = () => {
-    return (
-        <ContentLoader
+interface ILoader {
+  title?: string;
+}
+
+export const LoaderComponent: FC<ILoader> = ({ title }) => {
+  return (
+    <div className='loader'>
+      <div className='container'>
+        <h2 className='title'>{title}</h2>
+        <div className='loader__wrapper'>
+          <ContentLoader
             speed={2}
             width={1196}
             height={331}
-            viewBox="0 0 1196 331"
-            backgroundColor="#f3f3f3"
-            foregroundColor="#ecebeb"
-        >
-            <rect x="0" y="0" rx="0" ry="0" width="446" height="126" />
-            <rect x="464" y="1" rx="0" ry="0" width="689" height="290" />
-            <rect x="0" y="154" rx="0" ry="0" width="253" height="16" />
-            <rect x="0" y="190" rx="0" ry="0" width="252" height="14" />
-            <rect x="0" y="233" rx="0" ry="0" width="252" height="15" />
-            <rect x="0" y="272" rx="0" ry="0" width="251" height="12" />
-        </ContentLoader>
-    );
+            viewBox='0 0 1196 331'
+            backgroundColor='#f3f3f3'
+            foregroundColor='#ecebeb'
+          >
+            <rect x='0' y='0' rx='0' ry='0' width='446' height='126' />
+            <rect x='464' y='1' rx='0' ry='0' width='689' height='290' />
+            <rect x='0' y='154' rx='0' ry='0' width='253' height='16' />
+            <rect x='0' y='190' rx='0' ry='0' width='252' height='14' />
+            <rect x='0' y='233' rx='0' ry='0' width='252' height='15' />
+            <rect x='0' y='272' rx='0' ry='0' width='251' height='12' />
+          </ContentLoader>
+        </div>
+      </div>
+    </div>
+  );
 };
-
--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,22 +1,23 @@
-import './app.scss'
-import loadable from '@loadable/component'
+import './app.scss';
+import loadable from '@loadable/component';
 
-import {Route, Routes} from "react-router-dom";
-const Content = loadable(() => import("@/components/pages/Content/Content"))
-const CardItem = loadable(() => import('@/components/pages/CardItem/CardItem'))
-import {Layout} from "../UI/Layout/Layout";
-import {LoaderComponent} from "@/components/UI/LoaderComponent/LoaderComponent";
+import { Route, Routes } from 'react-router-dom';
+import { Layout } from '../UI/Layout/Layout';
+import { LoaderComponent } from '@/components/UI/LoaderComponent/LoaderComponent';
+
+const Content = loadable(() => import('@/components/pages/Content/Content'));
+const CardItem = loadable(() => import('@/components/pages/CardItem/CardItem'));
 
 function App() {
   return (
     <Layout>
       <Routes>
-        <Route path={'/'} element={<Content fallback={<LoaderComponent/>}/>}/>
-        <Route path={'/vacancies/:id'} element={<CardItem/>}/>
+        <Route path={'/'} element={<Content fallback={<LoaderComponent title='List of vacancies' />} />} />
+        <Route path={'/vacancies/:id'} element={<CardItem />} />
         <Route path={'/favourites'} />
       </Routes>
     </Layout>
-  )
+  );
 }
 
-export default App
+export default App;
