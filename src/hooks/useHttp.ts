e0a2163f86b204d1dbe11b487640a067b311793f
Yaroslav <yroslav070411@gmail.com>
2023-02-27 22:18:27 +0300

    Before changes

--- src/hooks/useHttp.ts
+++ src/hooks/useHttp.ts
@@ -3,11 +3,24 @@ import axios from 'axios';
 
 axios.defaults.baseURL = `https://api.hh.ru/`;
 
+interface IGetVacancies {
+  page: number;
+  onPage?: number;
+}
+
 export const useHttp = () => {
   const { perPage } = useTypedSelector((state) => state.pagination);
 
-  const getVacancies = async (url: string, { perPage: number = perPage }) => {
+  const getVacancies = async (url: string, { page }: IGetVacancies) => {
     try {
+      const response = await axios.get(`vacancies/`, {
+        params: {
+          salary: 30000,
+          currency: 'RUR',
+          per_page: perPage,
+          page,
+        },
+      });
     } catch (e) {}
   };
 };
