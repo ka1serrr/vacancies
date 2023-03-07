366f846dac4fa7b81f27d371dd131f61f6c5fa08
ka1serrr <86408343+ka1serrr@users.noreply.github.com>
2023-03-08 01:43:13 +0300

    minor changes

--- src/imports/hooks.ts
+++ src/imports/hooks.ts
@@ -1,15 +1,9 @@
-import {useActions} from "@/hooks/useActions";
-import {useTypedSelector} from "@/hooks/useTypedSelector";
-import {useItems} from "@/hooks/useItems";
-import {useTitle} from 'react-use';
-import {useParams} from "react-router-dom";
-import {useSalary} from "@/hooks/useSalary";
+import { useActions } from '@/hooks/useActions';
+import { useTypedSelector } from '@/hooks/useTypedSelector';
+import { useItems } from '@/hooks/useItems';
+import { useTitle } from 'react-use';
+import { useParams } from 'react-router-dom';
+import { useSalary } from '@/hooks/useSalary';
+import { useLocalStorage } from '@/hooks/useLocalStorage';
 
-export {
-  useItems,
-  useActions,
-  useTypedSelector,
-  useTitle,
-  useParams,
-  useSalary
-}
+export { useItems, useActions, useTypedSelector, useTitle, useParams, useSalary, useLocalStorage };
