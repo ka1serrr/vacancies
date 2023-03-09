3215dee391a8e525d3921391ca270eaf5d5925c1
Yaroslav <yroslav070411@gmail.com>
2023-03-10 01:53:56 +0300

    minor

--- src/components/common/Reg/Reg.tsx
+++ src/components/common/Reg/Reg.tsx
@@ -1,8 +1,12 @@
+import { Form } from '@/components/UI/Form/Form';
+
 export const Reg = () => {
+  const handleClick = () => {
+    console.log('submit');
+  };
   return (
     <div>
-      
+      <Form btnText='Registration' handleClick={handleClick}></Form>
     </div>
   );
 };
-
