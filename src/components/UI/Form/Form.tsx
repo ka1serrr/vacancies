c1f92a4be38482b4de5fddafd0a9c2983ef66b11
Yaroslav <yroslav070411@gmail.com>
2023-03-09 17:11:52 +0300

    Added styles for form

--- src/components/UI/Form/Form.tsx
+++ src/components/UI/Form/Form.tsx
@@ -3,6 +3,7 @@ import { FC } from 'react';
 import firebase from 'firebase/compat';
 import functions = firebase.functions;
 import { Button } from '@/components/UI/Button/Button';
+import './form.scss';
 
 interface IForm {
   btnText: string;
@@ -21,27 +22,33 @@ export const Form: FC<IForm> = ({ btnText, handleClick }) => {
     <form className='form' onSubmit={formik.handleSubmit}>
       <div className='container'>
         <div className='form__wrapper'>
-          <label htmlFor='email' className='form__label'>
-            Enter your email
-          </label>
-          <input
-            type='email'
-            id='email;'
-            name='email'
-            className='form__input'
-            value={formik.values.email}
-            onBlur={formik.handleBlur}
-            onChange={formik.handleChange}
-          />
-          <input
-            type='password'
-            id='password'
-            name='password'
-            placeholder='Password'
-            value={formik.values.password}
-            onBlur={formik.handleBlur}
-            onChange={formik.handleChange}
-          />
+          <div className='form__inputs-wrap'>
+            <label htmlFor='email' className='form__label'>
+              Email
+            </label>
+            <input
+              type='email'
+              id='email;'
+              name='email'
+              className='form__input'
+              value={formik.values.email}
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+            />
+            <label htmlFor='password' className='form__label'>
+              Password
+            </label>
+            <input
+              type='password'
+              id='password'
+              name='password'
+              placeholder='Password'
+              className='form__input'
+              value={formik.values.password}
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+            />
+          </div>
         </div>
         <Button title={btnText} className='form__btn' />
       </div>
